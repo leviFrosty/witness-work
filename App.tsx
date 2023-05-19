a728a0587999f611df0ec275586769a5ef8c4663
Levi Wilkerson <chelleas123@gmail.com>
2023-05-18 21:19:10 -0400

    feat: add Inter-Regular font, splash screen, and app icons

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+import Entypo from "@expo/vector-icons/Entypo";
+import { useFonts } from "expo-font";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +15,36 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    fontFamily: "Inter-Regular",
+  },
+  font: {
+    fontFamily: "Inter-Regular",
   },
 });
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Inter-Regular": require("./assets/fonts/Inter-Regular.otf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+        <Text style={styles.font}>Hello World!</Text>
+        <Text style={styles.font}>Hello World!</Text>
+        <StatusBar style="auto" />
+        <Entypo name="rocket" size={30} color="#AC94C9" />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}

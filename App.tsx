61f441f592b826b6aacf871a4b6612c0752d2c28
Levi Wilkerson <chelleas123@gmail.com>
2023-05-27 16:27:32 -0400

    feat: add react native paper for MUI

--- App.tsx
+++ App.tsx
@@ -5,6 +5,12 @@ import { useCallback } from "react";
 import Entypo from "@expo/vector-icons/Entypo";
 import { useFonts } from "expo-font";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
@@ -37,6 +43,18 @@ export default function App() {
     "Inter-Regular": require("./assets/fonts/Inter-Regular.otf"),
   });
   const colorScheme = useColorScheme();
+  const randomWidth = useSharedValue(10);
+
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const ViewStyle = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
 
   const themeTextStyle =
     colorScheme === "light" ? styles.lightThemeText : styles.darkThemeText;
@@ -61,11 +79,13 @@ export default function App() {
         style={[styles.container, themeContainerStyle]}
         onLayout={onLayoutRootView}
       >
-        <Text style={[styles.text, themeTextStyle]}>
-          Color scheme: {colorScheme}
-        </Text>
-        <StatusBar style="auto" />
-        <Entypo name="rocket" size={30} color="#AC94C9" />
+        <Animated.View style={ViewStyle}>
+          <Text style={[styles.text, themeTextStyle]}>
+            Color scheme: {colorScheme}
+          </Text>
+          <StatusBar style="auto" />
+          <Entypo name="rocket" size={30} color="#AC94C9" />
+        </Animated.View>
       </SafeAreaView>
     </SafeAreaProvider>
   );

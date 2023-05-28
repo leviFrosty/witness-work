95c11daf4a10beac57bfed58135bee79304985d2
Levi Wilkerson <chelleas123@gmail.com>
2023-05-27 23:51:34 -0400

    feat: add navigation and paper theme

--- App.tsx
+++ App.tsx
@@ -1,92 +1,79 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, useColorScheme } from "react-native";
+import { View, useColorScheme } from "react-native";
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 import * as SplashScreen from "expo-splash-screen";
+import {
+  Text,
+  PaperProvider,
+  Button,
+  MD3DarkTheme,
+  MD3LightTheme,
+  adaptNavigationTheme,
+} from "react-native-paper";
 import { useCallback } from "react";
-import Entypo from "@expo/vector-icons/Entypo";
-import { useFonts } from "expo-font";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import Animated, {
-  Easing,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from "react-native-reanimated";
+import {
+  NavigationContainer,
+  DefaultTheme as NavigationDefaultTheme,
+  DarkTheme as NavigationDarkTheme,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
-const styles = StyleSheet.create({
-  lightThemeContainer: {
-    backgroundColor: "#ffffff",
-  },
-  darkThemeContainer: {
-    backgroundColor: "#242c40",
-  },
-  lightThemeText: {
-    color: "#242c40",
-  },
-  darkThemeText: {
-    color: "#ffffff",
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontFamily: "Inter-Regular",
-  },
-});
-
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    "Inter-Regular": require("./assets/fonts/Inter-Regular.otf"),
-  });
-  const colorScheme = useColorScheme();
-  const randomWidth = useSharedValue(10);
-
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const ViewStyle = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
-
-  const themeTextStyle =
-    colorScheme === "light" ? styles.lightThemeText : styles.darkThemeText;
-  const themeContainerStyle =
-    colorScheme === "light"
-      ? styles.lightThemeContainer
-      : styles.darkThemeContainer;
-
   const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
+    await SplashScreen.hideAsync();
+  }, []);
+  const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme();
+  const paperTheme =
+    colorScheme === "dark"
+      ? { ...MD3DarkTheme, colors: theme.dark }
+      : { ...MD3LightTheme, colors: theme.light };
+  const { DarkTheme, LightTheme } = adaptNavigationTheme({
+    reactNavigationLight: NavigationDefaultTheme,
+    reactNavigationDark: NavigationDarkTheme,
+  });
+  const Stack = createNativeStackNavigator();
 
-  if (!fontsLoaded) {
-    return null;
+  function HomeScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+        <Button onPress={() => navigation.navigate("Details")}>Details</Button>
+      </View>
+    );
+  }
+  function DetailsScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
   }
 
+  SplashScreen.hideAsync();
+
   return (
-    <SafeAreaProvider>
-      <SafeAreaView
-        style={[styles.container, themeContainerStyle]}
-        onLayout={onLayoutRootView}
+    <PaperProvider theme={paperTheme}>
+      {/* <SafeAreaView onLayout={onLayoutRootView}> */}
+      <NavigationContainer
+        theme={colorScheme === "dark" ? DarkTheme : LightTheme}
       >
-        <Animated.View style={ViewStyle}>
-          <Text style={[styles.text, themeTextStyle]}>
-            Color scheme: {colorScheme}
-          </Text>
-          <StatusBar style="auto" />
-          <Entypo name="rocket" size={30} color="#AC94C9" />
-        </Animated.View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={
+              {
+                // headerShown: false,
+              }
+            }
+          />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      {/* </SafeAreaView> */}
+    </PaperProvider>
   );
 }

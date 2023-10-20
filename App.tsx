783f6757e696629edd874381181d4738e097242a
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2023-10-20 01:53:00 -0400

    feat: display notifications while application is open

--- App.tsx
+++ App.tsx
@@ -3,6 +3,15 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import RootStackComponent from "./src/stacks/RootStack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
   return (

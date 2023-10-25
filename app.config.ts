528e3063800aba2fb54167037a5484e60fd6e58d
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2023-10-25 02:25:19 -0400

    chore: new version

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  slug: "jw-time",
   name: "JW Time",
+  slug: "jw-time",
   version: "1.0.0",
   orientation: "portrait",
   icon: "./src/assets/icon.png",
@@ -19,7 +19,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     bundleIdentifier: "com.leviwilkerson.jwtime",
     infoPlist: {
       RCTAsyncStorageExcludeFromBackup: false,
-      CFBundleDisplayName: "JW Time",
     },
   },
   android: {
@@ -46,14 +45,13 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     url: "https://u.expo.dev/a67257dc-2fb8-4942-97f2-e9364b80d318",
   },
   plugins: ["sentry-expo", "expo-localization"],
-
   hooks: {
     postPublish: [
       {
         file: "sentry-expo/upload-sourcemaps",
         config: {
-          organization: process.env.EXPO_PUBLIC_SENTRY_ORG,
-          project: process.env.EXPO_PUBLIC_SENTRY_PROJECT,
+          organization: "levi-wilkerson",
+          project: "jw-time",
           setCommits: true,
         },
       },

9940ce200d068a5a7f5c363a1f913a6191d6c212
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2024-01-09 10:54:20 -0500

    chore: change bug report & feature requests to go through Github directly instead of email

--- src/screens/settings/sections/Contact.tsx
+++ src/screens/settings/sections/Contact.tsx
@@ -7,11 +7,10 @@ import {
   faChevronRight,
   faHand,
 } from '@fortawesome/free-solid-svg-icons'
-import Constants from 'expo-constants'
-import * as Device from 'expo-device'
 import IconButton from '../../../components/IconButton'
 import SectionTitle from '../shared/SectionTitle'
 import { openURL } from '../../../lib/links'
+import links from '../../../constants/links'
 
 const ContactSection = () => {
   return (
@@ -22,20 +21,7 @@ const ContactSection = () => {
           leftIcon={faBug}
           label={i18n.t('bugReport')}
           onPress={async () => {
-            const email = 'levi.wilkerson@proton.me'
-            const subjectText = '[JW Time] Bug Report'
-            const bodyText = `App Version: v${Constants.expoConfig
-              ?.version}, Device: ${Device.modelName}, OS: ${
-              Device.osVersion
-            }. ${i18n.t('pleaseDescribeYourIssue')}: --------------`
-            const subject = encodeURIComponent(subjectText)
-            const body = encodeURIComponent(bodyText)
-
-            openURL(`mailto:${email}?subject=${subject}&body=${body}`, {
-              alert: {
-                description: i18n.t('failedToOpenMailApplication'),
-              },
-            })
+            openURL(links.bugReport)
           }}
         >
           <IconButton icon={faChevronRight} />
@@ -45,20 +31,7 @@ const ContactSection = () => {
           leftIcon={faHand}
           label={i18n.t('featureRequest')}
           onPress={async () => {
-            const email = 'levi.wilkerson@proton.me'
-            const subjectText = '[JW Time] Feature Request'
-            const bodyText = `App Version: v${Constants.expoConfig
-              ?.version}, Device: ${Device.modelName}, OS: ${
-              Device.osVersion
-            }. ${i18n.t('pleaseDescribeYourFeatureClearly')}: --------------`
-            const subject = encodeURIComponent(subjectText)
-            const body = encodeURIComponent(bodyText)
-
-            openURL(`mailto:${email}?subject=${subject}&body=${body}`, {
-              alert: {
-                description: i18n.t('failedToOpenMailApplication'),
-              },
-            })
+            openURL(links.featureRequest)
           }}
         >
           <IconButton icon={faChevronRight} />

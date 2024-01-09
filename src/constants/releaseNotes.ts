d67bab316d7bb01e2f6995cf9f3f256c0fa0d0c5
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2024-01-09 11:39:02 -0500

    docs: add release notes

--- src/constants/releaseNotes.ts
+++ src/constants/releaseNotes.ts
@@ -9,6 +9,11 @@ export type ReleaseNote = {
 
 export const releaseNotes: ReleaseNote[] = [
   {
+    version: '1.11.0',
+    date: moment('2024-01-09').toDate(),
+    content: ['c1', 'c2'],
+  },
+  {
     version: '1.10.0',
     date: moment('2024-01-06').toDate(),
     content: ['c1', 'c2', 'c3'],

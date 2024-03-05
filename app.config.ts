5fb99c28efd6bac87f3678cbee6b16ac5e055159
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2024-03-04 23:49:30 -0500

    chore: upgrade version

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: IS_DEV ? 'JW Time Dev' : 'JW Time',
   developmentClient: {},
   slug: 'jw-time',
-  version: '1.22.0',
+  version: '1.22.1',
   owner: 'levi_frosty',
   orientation: 'portrait',
   icon: './src/assets/icon.png',

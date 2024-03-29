96ff8d822a08470d9decf06193a79e21a0c7d8ca
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2024-03-29 16:42:45 -0400

    fix: special pioneer does not have annual goal by default

--- src/hooks/usePublisher.ts
+++ src/hooks/usePublisher.ts
@@ -24,7 +24,7 @@ const usePublisher = (): PublisherDetails => {
       case 'regularPioneer':
         return true
       case 'specialPioneer':
-        return true
+        return false
       default:
         return true
     }

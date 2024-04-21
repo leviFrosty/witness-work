6f8f7dea8f366302e49ed4a1c7d1e610476bb8bf
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2024-04-21 00:12:33 -0400

    docs: update readme

--- src/constants/releaseNotes.ts
+++ src/constants/releaseNotes.ts
@@ -27,7 +27,7 @@ export const releaseNotes: ReleaseNote[] = [
   {
     version: '1.25.0',
     date: moment('2024-04-20').toDate(),
-    content: ['c1', 'c2', 'c3', 'c4'],
+    content: ['c1', 'c2', 'c5', 'c3', 'c4'],
   },
   {
     version: '1.24.0',

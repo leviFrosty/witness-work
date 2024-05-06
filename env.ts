858740326b22fb5481f9c695edc64187b86363b5
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2024-05-06 16:30:12 -0400

    chore: make zod env only run in dev mode

--- env.ts
+++ env.ts
@@ -1,20 +1,24 @@
 import { z } from 'zod'
 
 const envVariables = z.object({
-  GOOGLE_CLOUD_API_KEY: z.string().optional(),
-  HERE_API_KEY: z.string().optional(),
-  GOOGLE_MAPS_ANDROID_SDK_API_KEY: z.string().optional(),
-  REVENUECAT_APPLE_API_KEY: z.string().optional(),
-  SENTRY_AUTH_TOKEN: z.string().optional(),
-  EXPO_PUBLIC_SENTRY_PROJECT: z.string().optional(),
-  EXPO_PUBLIC_SENTRY_ORG: z.string().optional(),
+  GOOGLE_CLOUD_API_KEY: z.string(),
+  HERE_API_KEY: z.string(),
+  GOOGLE_MAPS_ANDROID_SDK_API_KEY: z.string(),
+  REVENUECAT_APPLE_API_KEY: z.string(),
+  SENTRY_AUTH_TOKEN: z.string(),
+  EXPO_PUBLIC_SENTRY_PROJECT: z.string(),
+  EXPO_PUBLIC_SENTRY_ORG: z.string(),
   APP_VARIANT: z
     .string()
     .optional()
     .describe('`development` is used to run the app in development mode.'),
 })
 
-envVariables.parse(process.env)
+if (__DEV__) {
+  // Only runtime checks environment variables for dev mode.
+  // Ensures production still launches always.
+  envVariables.parse(process.env)
+}
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace

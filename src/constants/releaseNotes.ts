09428c3eb7ad60b2bbfd77943dc02a04076a3d1a
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2024-07-23 23:11:52 -0400

    chore: update release notes

--- src/constants/releaseNotes.ts
+++ src/constants/releaseNotes.ts
@@ -27,7 +27,7 @@ export const releaseNotes: ReleaseNote[] = [
   {
     version: '1.30.0',
     date: moment('2024-07-24').toDate(),
-    content: ['c1'],
+    content: ['c1', 'c2'],
   },
   {
     version: '1.29.1',

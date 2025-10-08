5e9c6e8bb5ccc18af1c9fcee577fdf3b1e09233e
Levi Wilkerson <levi.wilkerson@proton.me>
2025-10-07 23:47:11 -0400

    chore: bump version to 1.37.0

--- app.config.ts
+++ app.config.ts
@@ -9,7 +9,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     name: IS_DEV ? 'WitnessWork Dev' : 'WitnessWork',
     developmentClient: {},
     slug: 'jw-time',
-    version: '1.36.0',
+    version: '1.37.0',
     owner: 'levi_frosty',
     orientation: 'portrait',
     icon: './src/assets/icon.png',

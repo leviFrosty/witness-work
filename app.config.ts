d0aea5ba7c4fe421f3713c3870c1da6c6173504d
Levi Wilkerson <levi@leviwilkerson.com>
2026-04-13 09:52:22 -0400

    chore: bump version to 1.38.2

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     name: IS_DEV ? 'WitnessWork Dev' : 'WitnessWork',
     developmentClient: {},
     slug: 'jw-time',
-    version: '1.38.1',
+    version: '1.38.2',
     owner: 'levi_frosty',
     scheme: 'witnesswork',
     orientation: 'portrait',

7831978ed65ad1194e73a385e05c5685cb839b05
Levi Wilkerson <levi@leviwilkerson.com>
2026-06-12 17:41:34 -0400

    chore: create release notes

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     name: IS_DEV ? 'WitnessWork Dev' : 'WitnessWork',
     developmentClient: {},
     slug: 'jw-time',
-    version: '1.38.3',
+    version: '1.40.0',
     owner: 'levi_frosty',
     scheme: 'witnesswork',
     orientation: 'portrait',

be02a8cd01965b1ad7ea0456eca2b7811fa0750d
Levi Wilkerson <levi@leviwilkerson.com>
2026-09-23 22:03:35 -0400

    fix: remove map color key helper text

--- src/features/map/components/MapColorKey.tsx
+++ src/features/map/components/MapColorKey.tsx
@@ -1,12 +1,5 @@
 import StalenessColorKey from '@/components/StalenessColorKey'
-import Text from '@/components/ui/MyText'
-import i18n from '@/lib/locales'
 
 export default function MapKey() {
-  return (
-    <>
-      <StalenessColorKey />
-      <Text style={{ marginTop: 12 }}>{i18n.t('map_dropPinHint')}</Text>
-    </>
-  )
+  return <StalenessColorKey />
 }
